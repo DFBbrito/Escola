3bd65a046d48e5a2d6eb801c2c76b0f4a6ffbb8c
DFBbrito <a24567@esenviseu.net>
2023-11-29 13:08:44 +0000

    new file:   M03/troco.py

---
+++ M03/troco.py
@@ -0,0 +1,20 @@
+from termcolor import colored
+
+
+def imprimir_texto_cor(texto,cor="white"):
+    print(colored(texto,cor))
+
+def Troco(pagar,dinheiro):
+    if dinheiro<=0 or dinheiro<pagar:
+        imprimir_texto_cor('Nao tem dinheiro suficiente')
+        return None
+    dinheiro-=pagar
+    imprimir_texto_cor(f'Pagou {pagar} e ficou com {dinheiro}','blue')
+    return dinheiro
+
+carteira=100
+carteira=Troco(50,carteira)
+if carteira!=None:
+    imprimir_texto_cor(f'Tem na carteira {carteira}','green')
+else:
+    imprimir_texto_cor('Nao tem dinheiro na carteira','red')
