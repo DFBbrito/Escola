d209c0af9cdbb81519614543ba4cea8b3218316b
DFBbrito <a24567@esenviseu.net>
2024-03-21 13:40:48 +0000

    mercado

--- M05/supermercados.py
+++ M05/supermercados.py
@@ -1,5 +1,73 @@
-auchan = {"frango":2.34,"arroz":1.05,"maça":1.59,"leite":0.80,"azeite":12.68,"agua_5l":1.05,"agua_6l":0.84,"sal_grosso":2.29,"salmao":24.93,"açucar":1.79,"cafe":6.59}
+#Programa para sugerir a loja onde um cabaz de produtos é mais barato
+#O utilizador insere um cabaz de produtos a comprar
+#Se não existirem todos os produtos a comprar deve informar o utilizador
 
-continente={"loja":{"Arroz":1.83,"Maçã":1.99,"Leite":0.82,"Frango":1.25,"Café":6.59,"Água5L":1.05,"Água6L":0.84}}
+precos={
+    "Pingo Doce":{"Arroz": 1.18, "Maçã": 2.85, "Leite": 0.82, "Frango": 4.98, "Café": 22.9, "Água5l": 1.29, "Azeite": 9.90, "Sal Grosso": 0.25, "Açucar": 7.30, "Salmão": 12.99},
 
-pingoDoce = {"Arroz": 1.18, "Maçã": 2.85, "Leite": 0.82, "Frango": 4.98, "Café": 36.98, "Água": 1.29, "Azeite": 9.90, "Sal Grosso": 0.25, "Açucar": 7.30, "Salmão": 12.99}
+    "Auchan":{"Frango":2.34,"Arroz":1.05,"Maçã":1.59,"Leite":0.80,"Azeite":12.68,"Água5l":1.05,"Água6l":0.84,"Sal Grosso":2.29,"Salmão":24.93,"Açucar":1.79,"Café":6.59},
+    
+    "Continente":{"Arroz":1.83,"Maçã":1.99,"Leite":0.82,"Frango":1.25,"Café":6.59,"Água5L":1.05,"Água6L":0.84}
+}
+
+
+def ProdutoExiste(produto):
+    for loja,produtos in precos.items():
+        if produto in produtos:
+            return True    
+    return False
+
+def QuantoCusta(loja,produto):
+    produtos=precos[loja]
+    if produtos in produtos:
+        return produtos[produto]
+    return None
+
+def LerCompras():
+    compras={}
+    while True:
+        produto=input("Introduza o nome do produto:").capitalize()
+        if produto=="":
+            break
+        if ProdutoExiste(produto)==False:
+            print("Produto não existente")
+            continue
+        quantidade=float(input("Quantida a comprar:"))
+        if quantidade<=0:
+            print("Quantidade deve ser superior a 0!")
+            continue
+        if produto in compras:
+            compras[produto]+=quantidade
+        else:
+            compras[produto]=quantidade
+    print(f"A sua lista de compras {compras}")
+    return compras
+
+def LojaMaisBarata(compras):
+    maisbarato=None
+    lojaBarata=None
+    for loja in precos.keys():
+        soma=0
+        for produto,quantidade in compras.items():
+            custo=QuantoCusta(loja,produto)
+            if custo is None:
+                print(f"A {loja} não tem {produto}")
+                soma=-1
+                break
+            soma+=custo*quantidade
+        if soma<0:
+            continue
+        if maisbarato is None or soma<maisbarato:
+            maisbarato=soma
+            lojaBarata=loja
+    return (lojaBarata,maisbarato)
+        
+
+def main():
+    compras=LerCompras()
+    loja,custo=LojaMaisBarata(compras)
+    print(f"Loja mais barata {loja} e as suas compras vão custar {custo}")
+
+
+if __name__=="__main__":
+    main()
